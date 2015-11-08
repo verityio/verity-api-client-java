16c21ad2895c9f3bd7d585e0221d7d9e2fd5b642
Donald Jackson <donald@centio.co>
2015-11-09 01:50:26 +0200

    Added simple script so I don't forget

---
+++ build-java-code.sh
@@ -0,0 +1,2 @@
+java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -c java-config.json -i "http://localhost:8080/Verity/api-docs" --skip-overwrite  -l java   -o .
+
